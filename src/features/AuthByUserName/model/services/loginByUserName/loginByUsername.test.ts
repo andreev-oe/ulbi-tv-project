533f0cadb4086905c0439fb75d173c2ac2a64c7b
Oleg Andreev <andreev.oe@yandex.ru>
2024-10-03 23:49:19 +0300

    fix: исправлены ошибки в тестах

--- src/features/AuthByUserName/model/services/loginByUserName/loginByUsername.test.ts
+++ src/features/AuthByUserName/model/services/loginByUserName/loginByUsername.test.ts
@@ -1,33 +1,29 @@
-import axios from 'axios';
 import { userActions } from 'entities/User';
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 
 import { loginByUsername } from './loginByUsername';
 
-jest.mock('axios');
-const mockedAxios = jest.mocked(axios, true);
-
 describe('loginByUsername.test', () => {
     test('Запрос выполнен успешно', async () => {
         const userData = { username: 'username', id: 'id' };
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userData }));
         const thunk = new TestAsyncThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(Promise.resolve({ data: userData }));
         const result = await thunk.callThunk({ username: 'username', password: 'password' });
 
         expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userData));
         expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('fulfilled');
         expect(result.payload).toEqual(userData);
     });
 
     test('Запрос выполнен, ответ код 403', async () => {
-        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const thunk = new TestAsyncThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const result = await thunk.callThunk({ username: 'username', password: 'password' });
 
         expect(thunk.dispatch).toHaveBeenCalledTimes(2);
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('rejected');
         expect(result.payload).toBe('Вы ввели неправильный логин или пароль');
     });
