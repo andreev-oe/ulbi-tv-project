8a5b605276727d28cb0115523b098db13a004f41
Oleg Andreev <andreev.oe@yandex.ru>
2024-10-10 00:15:01 +0300

    feat: добавлены стори для SideBar

--- src/widgets/SideBar/ui/SideBar/SideBar.stories.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Theme } from 'app/providers/themeProvider';
 import { GlobalThemeDecorator } from 'shared/config/storybook/decorators/GlobalThemeDecorator';
+import { ReduxStoreDecorator } from 'shared/config/storybook/decorators/ReduxStoreDecorator';
 
 import { SideBar } from './SideBar';
 
@@ -15,7 +16,25 @@ const Template: ComponentStory<typeof SideBar> = (args) => <SideBar {...args} />
 
 export const Light: ComponentStory<typeof SideBar> = Template.bind({});
 Light.args = {};
+Light.decorators = [
+    ReduxStoreDecorator({
+        user: { authData: {} },
+    }),
+];
 
 export const Dark: ComponentStory<typeof SideBar> = Template.bind({});
 Dark.args = {};
-Dark.decorators = [GlobalThemeDecorator(Theme.DARK)];
+Dark.decorators = [
+    GlobalThemeDecorator(Theme.DARK),
+    ReduxStoreDecorator({
+        user: { authData: {} },
+    }),
+];
+
+export const NoAuth: ComponentStory<typeof SideBar> = Template.bind({});
+NoAuth.args = {};
+NoAuth.decorators = [
+    ReduxStoreDecorator({
+        user: {},
+    }),
+];
