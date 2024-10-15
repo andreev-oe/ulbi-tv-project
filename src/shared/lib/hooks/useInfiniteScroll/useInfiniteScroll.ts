dcbc5612d335b3aaff6e411be68f6c29aa5beb34
Oleg Andreev <andreev.oe@yandex.ru>
2024-10-15 21:04:52 +0300

    feat: добавлен хук для подгрузки данных при скролле списка

---
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -0,0 +1,35 @@
+import { MutableRefObject, useEffect } from 'react';
+
+export interface IUseInfiniteScrollProps {
+    callBack?: () => void;
+    triggerRef: MutableRefObject<HTMLElement>;
+    wrapperRef: MutableRefObject<HTMLElement>;
+}
+
+export const useInfiniteScroll = ({ callBack, triggerRef, wrapperRef }: IUseInfiniteScrollProps) => {
+    useEffect(() => {
+        let observer: IntersectionObserver | null = null;
+
+        if (callBack) {
+            const options = {
+                root: wrapperRef.current,
+                rootMargin: '10px',
+                threshold: 1.0,
+            };
+
+            observer = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting) {
+                    callBack();
+                }
+            }, options);
+
+            observer.observe(triggerRef.current);
+        }
+
+        return () => {
+            if (observer) {
+                observer.disconnect();
+            }
+        };
+    }, []);
+};
--- src/shared/ui/Page/Page.tsx
+++ src/shared/ui/Page/Page.tsx
@@ -1,14 +1,27 @@
-import { memo, ReactNode } from 'react';
+import { memo, MutableRefObject, ReactNode, useRef } from 'react';
 
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
 
 import Styles from './Page.module.scss';
 
 interface IPageProps {
     className?: string;
     children: ReactNode;
+    onScrollEnd?: () => void;
 }
 
-export const Page = memo(({ className, children }: IPageProps) => {
-    return <div className={classNames({ rootClass: Styles.Page, additionalClasses: [className] })}>{children}</div>;
+export const Page = memo(({ className, children, onScrollEnd }: IPageProps) => {
+    const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+    useInfiniteScroll({ callBack: onScrollEnd, triggerRef: triggerRef, wrapperRef: wrapperRef });
+
+    return (
+        <div ref={wrapperRef} className={classNames({ rootClass: Styles.Page, additionalClasses: [className] })}>
+            {children}
+            <div ref={triggerRef} />
+        </div>
+    );
 });
